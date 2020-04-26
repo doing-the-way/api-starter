5d12146c89b6ad1e5e1b640d400acd4577ca0a41
franzZz <evenhold@gmail.com>
2020-04-26 12:19:35 -0500

    fix: errorResponse in home controller

--- src/resources/home/home.controllers.ts
+++ src/resources/home/home.controllers.ts
@@ -1,8 +1,15 @@
-export const me = (req, res) => {
-  res.status(200).json({ 
-    message: "Home router ..",
-    payload: req.payload,
-   })
+import ErrorResponse from '../../utils/errorResponse'
+
+export const me = (req, res, next) => {
+  try{
+    res.statusssss(200).json({ 
+      message: "Home router ..",
+      payload: req.payload,
+     })
+  }catch(err){
+    ErrorResponse(err, 'Error en controlador home', 500)
+    next(err)
+  }
   //  console.log('home method: ', req.method)
   //  console.log('home status: ', res.statusCode)
 }
